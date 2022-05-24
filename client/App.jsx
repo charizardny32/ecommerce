bf815cbca2a7ff9b30ddac87d99118eb9ce068b2
StanislausA <anthonystanislaus1@gmail.com>
2022-05-24 19:04:53 -0400

    pre merge

--- client/App.jsx
+++ client/App.jsx
@@ -5,7 +5,36 @@ function App() {
 	
 	return (
 		<>
-			<PayPalButtons fundingSource="paypal" />
+			<PayPalButtons
+				fundingSource="paypal"
+				createOrder={(data, actions) => {
+					console.log("order creation", data, actions);
+					return actions.order.create({
+						purchase_units: [
+							{
+								amount: {
+									value: "11.99"
+								}
+							}
+						]
+					});
+				}}
+				onApprove={(data, actions) => {
+					console.log("approved");
+					const name = actions.order.capture().then(details => details);
+					console.log("name:", name);
+					return name;
+				}}
+				onCancel={(data, actions) => {
+					const details = actions.order.capture().then(response => response);
+					console.log("order cancelled", details);
+					return "order cancelled";
+				}}
+				onError={() => {
+					console.log("order error");
+					return "order error";
+				}}
+			/>
 		</>
 	);
 }
--- client/index.jsx
+++ client/index.jsx
@@ -7,7 +7,11 @@ const container = document.getElementById("app");
 const root = createRoot(container);
 
 root.render(
-  <PayPalScriptProvider options={{ "client-id": "test", currency: "USD" }}>
+  <PayPalScriptProvider options={{
+    "client-id": "AQA9BFfPFPaFSpsp5BBLctmL5BMciKycnMf6s7quCwt4qt3hirbGNWXZDMKJOc4CoqYFRnF1yt7FEnKE",
+    currency: "USD"
+    }}
+    >
     <App />
   </PayPalScriptProvider>
 );
